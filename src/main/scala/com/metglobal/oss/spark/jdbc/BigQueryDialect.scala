27bf2b1c565da5e71ecaed44d87c404c5e61a006
Tarık Yılmaz <tarikyilmaz@yandex.com.tr>
2018-04-03 01:38:25 +0300

    Add standard sql prefix

--- src/main/scala/com/metglobal/oss/spark/jdbc/BigQueryDialect.scala
+++ src/main/scala/com/metglobal/oss/spark/jdbc/BigQueryDialect.scala
@@ -1,3 +1,20 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.metglobal.oss.spark.jdbc
 
 import java.sql.{JDBCType, SQLException, Types}
@@ -6,7 +23,11 @@ import org.apache.spark.sql.jdbc.JdbcDialect
 import org.apache.spark.sql.types._
 
 case object BigQueryDialect extends JdbcDialect {
+  val PREFIX = "#standardSQL\n"
+
   override def canHandle(url: String): Boolean = url.startsWith("jdbc:bigquery")
+
+  override def quoteIdentifier(colName: String): String = s"`$colName`"
 
   override def getCatalystType(sqlType: Int,
                                typeName: String,
@@ -23,9 +44,9 @@ case object BigQueryDialect extends JdbcDialect {
     decision
   }
 
-  override def getTableExistsQuery(table: String): String = {
-    s"SELECT * FROM $table WHERE 1=0"
-  }
+  override def getTableExistsQuery(table: String): String = s"$PREFIX SELECT 1 FROM $table LIMIT 1"
+
+  override def getSchemaQuery(table: String): String = s"$PREFIX SELECT * FROM $table LIMIT 1"
 
   override def isCascadingTruncateTable(): Option[Boolean] = Some(false)
 }
